8cb316c04ae9215ca6d8a416227bcd68757ca744
dvds <github-dvds@nym.hush.com>
2014-12-07 21:58:31 +0000

    Added shell script to create virtual python environment

---
+++ coverart-index-generator/generate-coverart-index.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# import virtualenv functions
+source "virtualenv-functions"
+
+# exit if virtual environment cannot be created
+if [ ! create_virtualenv_with_pip_requirements ]
+then
+    exit 1
+fi
+
+# exit if ghostscript is not installed
+if ! __check_apt_packages_installed ghostscript
+then
+    exit 1
+fi
+
+# declare variables
+OUTPUT_FILENAME="DVDs.ps"
+
+# generate the index file
+echo "TODO - generate the index"
+echo "%!PS" > "$OUTPUT_FILENAME"
+
+if [ $? -ne 0 ]
+then
+    # exit with failure
+    destroy_virtualenv
+    exit 1
+fi
+
+ps2pdf -dAutoFilterColorImages=false -dColorImageFilter=/FlateEncode "$OUTPUT_FILENAME"
+
+if [ $? -ne 0 ]
+then
+    # exit with failure
+    destroy_virtualenv
+    exit 1
+fi
+
+rm "$OUTPUT_FILENAME"
+
+if [ $? -ne 0 ]
+then
+    # exit with failure
+    destroy_virtualenv
+    exit 1
+fi
+
+# exit with success
+destroy_virtualenv
+exit 0
